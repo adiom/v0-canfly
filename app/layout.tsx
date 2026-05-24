1bbeeb96b6bdf364381b8bd72065ed5bbd1b3f47
Adiom Timur <adiom@canfly.org>
2026-05-25 00:14:06 +0300

    update для метрики 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
         />
         <noscript>
           <div>
-            <img src="https://mc.yandex.ru/watch/42420764" style="position:absolute; left:-9999px;" alt="" />
+            <img src="https://mc.yandex.ru/watch/42420764" style={{ position: 'absolute', left: '-9999px' }} alt="" />
           </div>
         </noscript>
       </body>
